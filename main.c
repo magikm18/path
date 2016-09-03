28110086915431dd78ecc48884fa134f61116f68
Nicholas Mertin <nickmertin@gmail.com>
2016-09-03 01:08:31 -0400

    Fixed typo

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-***************************************************************************
+/***************************************************************************
 * Copyright (c) 2016 Nicholas Mertin <nickmertin@gmail.com>
 *
 * This program is free software; you can redistribute it and/or modify
